fbefcb3ac9a6abd9e8ca9ad47e8716a41e613c18
leonace924 <meobius123@gmail.com>
2021-06-29 11:55:07 +0300

    Add hero and top blogs section for homepage

---
+++ src/sections/Home/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = ({ content }) => {
+  const { label, title, backgroundImage } = content;
+  return (
+    <div
+      className="flex flex-col items-center justify-center h-screen px-6 text-center bg-no-repeat bg-cover lg:px-20 md:px-12"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="mx-auto max-w-7xl">
+        <span className="text-base font-bold text-white uppercase lg:text-xl">{label}</span>
+        <h1 className="text-5xl leading-none text-white lg:text-6xl pt-3.5">{title}</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/sections/Home/TopBlogs/TopBlogs.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './TopBlogs.module.scss';
+
+const BlogCard = ({ blog, number }) => {
+  const { featuredImage, label, title, description } = blog;
+
+  return (
+    <div className={styles.blog}>
+      <div className={`${styles.blogImage} relative rounded overflow-hidden mb-5 lg:mb-10`}>
+        <Image src={featuredImage} alt={title} layout="fill" objectFit="cover" priority />
+      </div>
+      <span className="text-base font-bold text-black uppercase">{label}</span>
+      <h3 className="my-3 text-3xl text-black leading-extra-tight lg:my-5 lg:text-4xl">{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+const TopBlogs = ({ content }) => {
+  return (
+    <div className="px-6 mt-20 lg:px-20 md:px-12 lg:mt-36">
+      <div className="mx-auto max-w-7xl">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-5">
+          <div className="xl:col-start-1 xl:col-span-12 xl:grid-cols-12 lg:col-start-2 lg:col-span-10 lg:grid lg:grid-cols-10 lg:gap-5 lg:gap-y-25 lg:items-center">
+            {content.blogs?.map((blog, id) => (
+              <BlogCard blog={blog} number={id} key={id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBlogs;
