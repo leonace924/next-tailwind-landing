a05b361907749548618200f4581d61a98ed3fc47
leonace924 <meobius123@gmail.com>
2021-06-29 05:42:25 +0300

    Add extend styles to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
-  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/sections/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '4xl': '2.5rem',
+        '5xl': '3.125rem',
+        '6xl': '6.125rem',
+      },
+    },
   },
   variants: {
     extend: {},
