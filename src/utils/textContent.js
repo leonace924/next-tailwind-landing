b3af95a6513aabb1bfa83b1b70556fa5501901f5
leonace924 <meobius123@gmail.com>
2021-06-29 11:55:34 +0300

    Add text content

---
+++ src/utils/textContent.js
@@ -0,0 +1,41 @@
+const content = {
+  hero: {
+    backgroundImage: './images/img-hero-background.jpg',
+    label: 'Interview',
+    title: 'Catching up with Gold-Diggers',
+  },
+  topBlogs: {
+    blogs: [
+      {
+        featuredImage: '/images/img-blog-top-1.jpg',
+        label: 'Interview',
+        title: 'Catching up with Gold-Diggers',
+        description:
+          'Erat dui sem lectus facilisi augue nunc in. Tellus varius velit metus aliquam vel. Egestas purus aliquet enim quis. Lacus cras amet nunc, diam tincidunt mauris platea donec aliquam.',
+      },
+      {
+        featuredImage: '/images/img-blog-top-2.jpg',
+        label: 'Interview',
+        title: 'Yesterday, today, tomorrow: Sid Dog and year 2020',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tempor consectetur sit urna sed at a sed sapien a gravida.',
+      },
+      {
+        featuredImage: '/images/img-blog-top-3.jpg',
+        label: 'Interview',
+        title: 'Quis egestas eget elit sit sem amet et amet ac. Mauris sit dolor quam at.',
+        description:
+          'Pellentesque porttitor nullam sed ut cras in. Viverra euismod pretium mauris morbi. Tortor sed lobortis ultricies odio ultricies fringilla. Dolor eget risus ac in augue in at vel aenean.',
+      },
+      {
+        featuredImage: '/images/img-blog-top-4.jpg',
+        label: 'Company',
+        title: 'Blandit vitae quam nullam morbi. Urna elit lectus tristique tellus nec. Sed.',
+        description:
+          'Vulputate pellentesque orci tellus in vivamus gravida aenean id. Porttitor lacinia vestibulum leo mattis non consectetur leo. Turpis dignissim rutrum tristique libero. Nulla proin enim.',
+      },
+    ],
+  },
+};
+
+export default content;
