64458f3a6f95dc34dcd1513b2a1a7d0fcd651f67
leonace924 <meobius123@gmail.com>
2021-06-30 11:47:22 +0300

    Update top and mid blogs section

--- src/sections/Home/TopBlogs/TopBlogs.js
+++ src/sections/Home/TopBlogs/TopBlogs.js
@@ -7,7 +7,7 @@ const TopBlogs = ({ content }) => {
     <div className="px-6 mt-20 mb-12 lg:px-20 md:px-12 lg:mt-36 lg:mb-25">
       <div className="w-full mx-auto max-w-7xl">
         <div className="lg:grid lg:grid-cols-12 lg:gap-5">
-          <div className="xl:col-start-1 xl:col-span-12 xl:grid-cols-12 lg:col-start-2 lg:col-span-10 lg:grid lg:grid-cols-10 lg:gap-5 lg:gap-y-25 lg:items-center">
+          <div className="xl:col-start-2 xl:col-span-10 xl:grid-cols-10 lg:col-start-1 lg:col-span-12 lg:grid lg:grid-cols-12 lg:gap-5 lg:gap-y-25 lg:items-center">
             {content.blogs?.map((blog, id) => (
               <div className={styles.blog} key={id}>
                 <BlogCard blog={blog} portrait={id === 1 || id === 2} />
