a42fb02531d54f6e223431bd54fc8be00a9c68a5
leonace924 <meobius123@gmail.com>
2021-07-05 16:59:27 +0300

    Add round text component

---
+++ src/components/RoundText/RoundText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RoundText = ({ children }) => {
+  return (
+    <div className="px-6 py-1 my-2 mr-4 text-sm font-bold uppercase border border-black rounded-full lg:mr-6 lg:text-base lg:py-3 lg:my-3">
+      {children}
+    </div>
+  );
+};
+
+export default RoundText;
